507b10a00037fd3361a2e84278202b068bb9d4e5
Caleb Mucheru <mucherucaleb7@gmail.com>
2024-05-17 21:10:10 +0300

    Style Cards color

--- src/components/StartYourJourney.js
+++ src/components/StartYourJourney.js
@@ -5,9 +5,10 @@ import img3 from "../assets/img3.png";
 import '../styles/StartYourJourney.css'; 
 import JoinIcon from "../assets/join-now-icon.svg";
 
-function Card({ numberIcon, headline, description, imageUrl }) {
+function Card({ numberIcon, headline, description, imageUrl,backgroundColor }) {
     return (
-        <div className="card-container">
+        <div className="card-container" style={{ backgroundColor: backgroundColor }}>
+      
             <div className="left-card">
                 <div className="number-icon" style={{ fontSize: "30px" }}>{numberIcon}</div>
                 <h3 className="headline">{headline}</h3>
@@ -32,18 +33,22 @@ function ThreeCards() {
                 headline="Find Your Next Star Performer"
                 description="Explore the vast Zwilt marketplace to find the candidate that meets your needs"
                 imageUrl={img1}
+                backgroundColor="#EDEFFF"
             />
             <Card
                 numberIcon="2"
                 headline="Evaluate to Your Heart's Content"
-                description="Evaluate your heart's content and satisfaction."
+                description="Text 2 - A brief description goes here."
                 imageUrl={img2}
+                backgroundColor="#FFF7E1" 
             />
             <Card
                 numberIcon="3"
                 headline="Start Building Your Team"
                 description="Onboard your candidate right away and start creating the next big thing."
                 imageUrl={img3}
+
+                backgroundColor="#F5F5F5"
             />
         </div>
     );
